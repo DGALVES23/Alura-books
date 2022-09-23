117c1ff623975035fab42bb27c1cebb853f09fdb
DGALVES23 <99363692+DGALVES23@users.noreply.github.com>
2022-09-23 13:13:24 +0200

    implementacao dos filtros para livros disponiveis

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -3,18 +3,35 @@ const elementoParaInserirLivros = document.getElementById('livros');
 function exibirOsLivrosNaTela(listaDeLivros) {
       elementoParaInserirLivros.innerHTML = ''
       listaDeLivros.forEach(livro => {
-            elementoParaInserirLivros.innerHTML += `
-      <div class="livro">
-            <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-            <h2 class="livro__titulo">${livro.titulo}
-            </h2>
-            <p class="livro__descricao">${livro.autor}</p>
-            <p class="livro__preco" id="preco">R$ ${livro.preco.toFixed(2)}</p>
-            <div class="tags">
-            <span class="tag">Categoria: ${livro.categoria}</span>
-            </div>
+            /*let disponibilidade = verificarDisponibilidadeDoLivro(livro)*/
+            //operador ternário
+            let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel'
 
-    </div>
-            `
+            elementoParaInserirLivros.innerHTML += `
+
+            <div class="livro">
+                        <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
+                        <h2 class="livro__titulo">${livro.titulo}</h2>
+                        <p class="livro__descricao">${livro.autor}</p>
+                        <p class="livro__preco" id="preco">R$ ${livro.preco.toFixed(2)}</p>
+                  <div class="tags">
+                        <span class="tag">Categoria: ${livro.categoria}</span>
+                  </div>
+
+            </div>`
       });
 }
+
+/*i f tradicional
+function verificarDisponibilidadeDoLivro(livro) {
+      
+      if (livro.quantidade > 0) {
+            return 'livro__imagens'            
+      } else {
+            return 'livro__imagens indisponivel'
+      }
+}*/
+
+
+//Operador Ternário
+
